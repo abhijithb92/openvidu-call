ac5638d322e37e4fba1ef0a406d9c81837559472
abhijithb92 <abhijithb21@disroot.org>
2020-03-07 11:27:37 +0000

    Sample Recording Demo

--- front/openvidu-call/src/app/shared/components/toolbar/toolbar.component.ts
+++ front/openvidu-call/src/app/shared/components/toolbar/toolbar.component.ts
@@ -2,13 +2,26 @@ import { Component, OnInit, Input, EventEmitter, Output, HostListener } from '@a
 import { UserModel } from '../../models/user-model';
 import { ApiService } from '../../services/api.service';
 import { OvSettings } from '../../models/ov-settings';
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
+
+declare var require: any
+const FileSaver = require('file-saver');
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css'],
 })
+
 export class ToolbarComponent implements OnInit {
+
+  private record;
+    //Will use this flag for detect recording
+    private recording = false;
+    //Url of Blob
+    private url;
+    private error;
   fullscreenIcon = 'fullscreen';
 
   @Input() lightTheme: boolean;
@@ -26,7 +39,11 @@ export class ToolbarComponent implements OnInit {
   @Output() chatButtonClicked = new EventEmitter<any>();
   @Output() stopScreenSharingClicked = new EventEmitter<any>();
 
-  constructor(private apiSrv: ApiService) {}
+  constructor(private apiSrv: ApiService, private domSanitizer: DomSanitizer) {}
+
+  sanitize(url:string){
+        return this.domSanitizer.bypassSecurityTrustUrl(url);
+    }
 
   @HostListener('window:resize', ['$event'])
   sizeChange(event) {
@@ -73,4 +90,51 @@ export class ToolbarComponent implements OnInit {
       this.fullscreenIcon = 'fullscreen';
     }
   }
+  initiateRecording() {
+        
+        this.recording = true;
+        let mediaConstraints = {
+            video: false,
+            audio: true
+        };
+        navigator.mediaDevices
+            .getUserMedia(mediaConstraints)
+            .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+    }
+    /**
+     * Will be called automatically.
+     */
+    successCallback(stream) {
+        var options = {
+            mimeType: "audio/wav",
+            numberOfAudioChannels: 2
+        };
+        //Start Actuall Recording
+        var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+        this.record = new StereoAudioRecorder(stream, options);
+        this.record.record();
+    }
+    /**
+     * Stop recording.
+     */
+    stopRecording() {
+        this.recording = false;
+        this.record.stop(this.processRecording.bind(this));
+    }
+    /**
+     * processRecording Do what ever you want with blob
+     * @param  {any} blob Blog
+     */
+    processRecording(blob) {
+        this.url = URL.createObjectURL(blob);
+        FileSaver.saveAs(this.url)
+    }
+    /**
+     * Process Error.
+     */
+    errorCallback(error) {
+        this.error = 'Can not play audio in your browser';
+    }
 }
+
+
